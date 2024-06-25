47f04674e7e05f997a8f5f2eed3018af91bc698e
Nebo Milic <nebo.milic@gmail.com>
2024-06-25 12:24:58 +0200

    Remove unused route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, { Express } from 'express';
-import projectsRoute from './routes/projects.route';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,8 +7,6 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.use('/projects', projectsRoute);
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
