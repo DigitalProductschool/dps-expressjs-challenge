bff6c2aa6c5b476da904beae90fa067623ecc3e9
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-07-24 16:57:33 +0300

    feat: Implement project controllers and routes
    - Create a controller file to hold all project controllers
    - Create a routes file to hold all project routes
    - Implement the first endpoint: GET /projects to retrieve all projects as JSON

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import projectRoutes from './routes/project.routes';
 
 dotenv.config();
 
@@ -7,6 +8,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/projects', projectRoutes);
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
