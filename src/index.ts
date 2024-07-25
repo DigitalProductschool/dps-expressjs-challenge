a308051b725f9cce7c7f245d6a9b91a62dcda1e7
Tejas013 <tejasmadhukar6@gmail.com>
2024-07-25 18:34:21 +0530

    Added api versioning

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import projectRouter from './routes/projectRoutes';
+import reportRouter from './routes/reportRoutes';
 
 dotenv.config();
 
@@ -7,6 +9,11 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api/v1/projects', projectRouter);
+app.use('/api/v1/reports', reportRouter);
+app.get('/', (req, res) => {
+	res.send('Api Healthy');
+});
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
